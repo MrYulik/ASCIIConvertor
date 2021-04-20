2cc3f1e5c2e0220ec58d51f001be51e968394a59
MrYulik <73738033+MrYulik@users.noreply.github.com>
2021-04-20 14:03:04 +0300

    Add files via upload

---
+++ ASCIIConvertor/BitmapASCIIConvertor.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+
+namespace ASCIIConvertor
+{
+    class BitmapASCIIConvertor
+    {
+        private readonly char[] _asciiChars = { '.', ',', ':', '+', '*', '?', '%', '$', '#', '@' };
+        private readonly char[] _asciiCharsNegative = { '@', '#', '$', '%', '?', '*', '+', ':', ',', '.' };
+        private readonly Bitmap _bitmap;
+
+        public BitmapASCIIConvertor(Bitmap bitmap)
+        {
+            _bitmap = bitmap;
+        }
+
+        public char[][] Convert()
+        {
+            return Convert(_asciiChars);
+        }
+
+        public char[][] ConvertNegative()
+        {
+            return Convert(_asciiCharsNegative);
+        }
+
+        private char[][] Convert(char[] asciiChars)
+        {
+            var result = new char[_bitmap.Height][];
+
+            try
+            {
+                for (int y = 0; y < _bitmap.Height; y++)
+                {
+                    result[y] = new char[_bitmap.Height];
+
+                    for (int x = 0; x < _bitmap.Width; x++)
+                    {
+                        int mapIndex = (int)Map(_bitmap.GetPixel(x, y).R, 0, 255, 0, asciiChars.Length - 1);
+                        result[y][x] = asciiChars[mapIndex];
+                    }
+                }
+            }
+            catch { }
+
+            return result;
+        }
+
+        private float Map(float valueToMap, float start1, float stop1, float start2, float stop2)
+        {
+            return ((valueToMap - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+        }
+    }
+}
---
+++ ASCIIConvertor/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace ASCIIConvertor
+{
+    class Program
+    {
+        #region Settings
+        private const double WIDTH_OFFSET = 1.7;
+        private const int MAX_WIDTH = 474;
+        private const double X_OFFSET = 1.0;
+        private const double Y_OFFSET = 1.1;
+        private const int MAX_HEIGHT = 600;
+        #endregion
+        //char[] _asciiChars = { '.', ',', ':', '+', '*', '?', '%', '$', '#', '@' };
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            var OpenIMGDialog = new OpenFileDialog
+            {
+                Filter = "Image | *.bmp; *.png; *.jpg; *.JPEG"
+            };
+
+            Console.WriteLine("Нажмите Enter чтоб продолжить...\n");
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                if (OpenIMGDialog.ShowDialog() != DialogResult.OK)
+                    continue;
+
+                Console.Clear();
+
+                var bitmap = new Bitmap(OpenIMGDialog.FileName);
+                bitmap = ResizeBitmap(bitmap);
+                bitmap.ReconstructPixel();
+
+                var convertor = new BitmapASCIIConvertor(bitmap);
+                var rows = convertor.Convert();
+
+                foreach (var row in rows)
+                    Console.WriteLine(row);
+
+                var rowNegative = convertor.ConvertNegative();
+                File.WriteAllLines("result.txt", rowNegative.Select(r => new string(r)));
+
+                Console.SetCursorPosition(0, 0);
+            }
+        }
+
+        private static Bitmap ResizeBitmap(Bitmap bitmap)
+        {
+            var maxWidth = MAX_WIDTH;
+            var counting = X_OFFSET + Y_OFFSET / MAX_HEIGHT;
+            var newHeight = bitmap.Height / WIDTH_OFFSET * maxWidth / bitmap.Width;
+            if (bitmap.Width > maxWidth || bitmap.Height > newHeight)
+                bitmap = new Bitmap(bitmap, new Size(maxWidth, (int)newHeight));
+            return bitmap;
+        }
+    }
+}
